11570536c64717297ec002de40161e04bd5a9443
Giacomo Vittori <114087341+JackVittori@users.noreply.github.com>
2024-04-27 15:33:08 +0200

    Add notebook to try to define a quantum layer as a keras layer

--- circuit_model.py
+++ circuit_model.py
@@ -1,14 +1,16 @@
-import tensorflow as tf
+import torch
 import pennylane as qml
 
 
+def subroutine(wires):
+    qml.
 class QuantumCircuit:
     def __init__(self, num_qubits:int, dev: str):
         self.n_qubits = num_qubits
         self.dev = qml.device(dev, wires = self.n_qubits)
 
-    def circuit_block(self, params, state=None):
-        if state is not None:
+    def layer(self, params, state=None):
+
 
 
 
