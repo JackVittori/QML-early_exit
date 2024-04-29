61bb52aada4634b6f04b8883d5576b9debeee24d
Giacomo Vittori <114087341+JackVittori@users.noreply.github.com>
2024-04-29 19:41:24 +0200

    update, but no progress. Try to use: TorchLayer or change require_grad on input parameters

--- circuit_model.py
+++ circuit_model.py
@@ -1,16 +1,30 @@
 import torch
 import pennylane as qml
+from torch.autograd import Variable
+class BasicLayer:
+    def __init__(self, n_qubits: int, params = None):
+        self.n_qubits = n_qubits
+        if params is None:
+            self.parameters = Variable(torch.normal(mean = 0, std = 0.1, size = (self.n_qubits, 3)), requires_grad = True)
 
+        for i in range(self.n_qubits):
+            qml.RX(self.parameters[i, 0], wires=i)
+            qml.RY(self.parameters[i, 1], wires=i)
+            qml.RZ(self.parameters[i, 2], wires=i)
 
-def subroutine(wires):
-    qml.
-class QuantumCircuit:
-    def __init__(self, num_qubits:int, dev: str):
-        self.n_qubits = num_qubits
-        self.dev = qml.device(dev, wires = self.n_qubits)
+        for i in range(self.n_qubits):
+            qml.CNOT(wires=[i, (i + 1) % self.n_qubits])
 
-    def layer(self, params, state=None):
+class CircuitModel:
+    def __init__(self, device: str, n_qubits: int):
+        self.n_qubits = n_qubits
+        self.device = device
 
+    def build(self, n_layers:int):
+        @qml.qnode(self.device, wires = self.n_qubits)
+        parameters = Variable(torch.normal( mean=0. , std=0.1, size=(n_layers, self.n_qubits, 3)), requires_grad=True)
+        for i in range(n_layers):
+        
 
 
 
