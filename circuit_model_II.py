671824b84f7324f3d443ad9efbdea1a9fd66cb42
Giacomo Vittori <114087341+JackVittori@users.noreply.github.com>
2024-05-06 21:38:27 +0200

    Add compute output using only some layers to fit function

--- circuit_model_II.py
+++ circuit_model_II.py
@@ -20,74 +20,105 @@ class QuantumCircuit(Module):
         self.num_layers = num_layers
         self.dev = qml.device("default.qubit", wires=num_qubits)
         self.params = torch.nn.ParameterDict({
-            f'layer_{i + 1}': torch.nn.Parameter(torch.rand(num_qubits, 3, requires_grad=True))
+            f'layer_{i}': torch.nn.Parameter(torch.rand(num_qubits, 3, requires_grad=True))
             for i in range(num_layers)
         })
 
         @qml.qnode(self.dev, interface=interface)
-        def _quantum_function(params: Dict, state: torch.Tensor) -> StateMP:
+        def _quantum_function(params: Dict, state: torch.Tensor,
+                              num_layers_to_execute: Optional[int] = None) -> StateMP:
             """
+            Execute the quantum circuit with specified parameters and initial state up to the given number of layers.
+
+            :param num_layers_to_execute: The number of layers to execute.
             :param params: Dictionary of parameters to be passed to the rotational gates.
             :param state: a tensor of shape (batch, 256) to initialize the circuit with the image (16x16).
             :return: the output state of the quantum circuit in the computational basis
 
             Each element of the state vector representing the quantum system is a complex number representing the
-            probability amplitude of the system being in one of the 256 = 2^8 states.
+            probability amplitude of the system being in one of the 256 = 2^8 states. Thus, data has to be normalized
+            before being passed to the quantum circuit.
             """
             if state is not None:
+                # state vector initialization with input
                 qml.QubitStateVector(state, wires=range(self.num_qubits))
 
-            for i in range(self.num_layers):
+            # execute only a specified number of layers or all layers
+            layers_to_execute = num_layers_to_execute if num_layers_to_execute is not None else self.num_layers
+
+            for i in range(layers_to_execute):
                 for j in range(self.num_qubits):
-                    qml.RX(params[f'layer_{i + 1}'][j, 0], wires=j)
-                    qml.RY(params[f'layer_{i + 1}'][j, 1], wires=j)
-                    qml.RZ(params[f'layer_{i + 1}'][j, 2], wires=j)
+                    qml.RX(params[f'layer_{i}'][j, 0], wires=j)
+                    qml.RY(params[f'layer_{i}'][j, 1], wires=j)
+                    qml.RZ(params[f'layer_{i}'][j, 2], wires=j)
                 for j in range(self.num_qubits):
                     qml.CNOT(wires=[j, (j + 1) % self.num_qubits])
             return qml.state()
 
         self.quantum_node = _quantum_function
 
-    def forward(self, state=None):
-        return self.quantum_node(self.params, state=state)
+    def forward(self, state: torch.Tensor = None, num_layers_to_execute: Optional[int] = None) -> StateMP:
+        """
+        Perform a forward pass on the quantum circuit.
+
+        :param state: a tensor of shape (batch, 256) to initialize the circuit with the image (16x16).
+        :param num_layers_to_execute: The number of layers to execute.
+        :return:
+        """
+        return self.quantum_node(params=self.params, state=state, num_layers_to_execute=num_layers_to_execute)
 
 
 class FullQuantumModel(Module):
-    """Full quantum model that integrates a quantum layer with measurement."""
+    """
+    Full quantum model that integrates a quantum layer with measurement.
+    """
 
     def __init__(self, qubits: int, layers: int):
         super().__init__()
         self.quantum_layer = QuantumCircuit(qubits, layers)
         self.params = self.quantum_layer.params
+        self.num_qubits = self.quantum_layer.num_qubits
+        self.num_layers = self.quantum_layer.num_layers
 
-    def forward(self, state: torch.Tensor):
-        """Calculate the probability distribution from quantum state measurements.
-        :param state: a tensor of shape (batch, 256) to initialize the circuit with the image (16x16).
-        :return: probabiity of being 1.
+    def forward(self, state: torch.Tensor, num_layers_to_execute: Optional[int] = None):
         """
-        state_vector = self.quantum_layer(state=state)
+        Calculate the probability distribution from quantum state measurements.
+
+        :param num_layers_to_execute: The number of layers to execute.
+        :param state: a tensor of shape (batch, 256) to initialize the circuit with the image (16x16).
+        :return: probability of being 1.
+        """
+        state_vector = self.quantum_layer(state=state, num_layers_to_execute=num_layers_to_execute)
         probabilities = torch.sum(torch.abs(state_vector[:, :2 ** (self.quantum_layer.num_qubits - 1)]) ** 2, dim=1)
         return probabilities.type(torch.float32)
 
     def trainable_layers(self):
-        """Prints layers and their trainability status."""
+        """
+        Prints layers and their trainability status.
+        """
         trainable_layers = dict()
         for layer in list(self.params.keys()):
             trainable_layers[layer] = self.params[layer].requires_grad
         print(trainable_layers)
 
     def trainable_parameters(self):
-        """Prints number of trainable parameters."""
+        """
+        Prints number of trainable parameters.
+        """
         print("Trainable parameters:", sum(p.numel() for p in self.parameters() if p.requires_grad == True))
 
     def get_trainable_params(self):
-        """Retrieve parameters that are marked as trainable.
+        """
+        Retrieve parameters that are marked as trainable.
+
         :return: iterable of parameter marked as trainable.
         """
         return iter([p for p in self.parameters() if p.requires_grad])
 
     def freeze_layers(self, layers: List[int]):
-        """Freeze specified layers from training.
+        """
+        Freeze specified layers from training.
+
         :param layers: List of indexes of layers to freeze.
         """
         valid_keys = list(self.params.keys())
@@ -97,13 +128,12 @@ class FullQuantumModel(Module):
             if layer_name not in valid_keys:
                 error_message = f"Please indicate the indexes of the layers choosing belong the following {valid_keys}"
                 raise ValueError(error_message)
-            if not self.params[layer_name].requires_grad:
-                warnings.warn(f"{layer_name} has already been frozen (requires_grad = False).")
             else:
                 self.params[layer_name].requires_grad = False
 
     def unfreeze_layers(self, layers: List[int]):
-        """Unfreeze specified layers for training.
+        """
+        Unfreeze specified layers for training.
 
         :param layers: List of indexes of layers that has to be set to non trainable.
 
@@ -115,13 +145,12 @@ class FullQuantumModel(Module):
             if layer_name not in valid_keys:
                 error_message = f"Please indicate the indexes of the layers choosing belong the following {valid_keys}"
                 raise ValueError(error_message)
-            if self.params[layer_name].requires_grad:
-                warnings.warn(f"{layer_name} is already trainable (requires_grad = True).")
             else:
                 self.params[layer_name].requires_grad = True
 
     def draw(self, style: str = 'default'):
-        """Draws the quantum circuit with specified style.
+        """
+        Draw the quantum circuit with specified style.
 
         :param style: style of drawing circuit.
         """
@@ -137,15 +166,23 @@ class FullQuantumModel(Module):
         plt.show()
 
     def fit(self, dataloader: DataLoader, learning_rate: float, epochs: int,
-            loss_function: torch.nn.modules.loss = torch.nn.BCELoss()) -> tuple:
+            loss_function: torch.nn.modules.loss = torch.nn.BCELoss(),
+            num_layers_to_execute: Optional[int] = None) -> tuple:
         """
+        Train the quantum circuit given a set of training data.
 
         :param dataloader: dataloader with training data.
         :param learning_rate: learning rate of the optimizer.
         :param epochs: number of epochs.
         :param loss_function: loss function.
+        :param num_layers_to_execute: The number of layers to execute
         :return: tuple containing average time per epoch and loss history.
         """
+
+        if num_layers_to_execute is not None:
+            self.unfreeze_layers(list(range(self.num_layers)))
+            self.freeze_layers([element for element in range(self.num_layers) if element not in range(num_layers_to_execute)])
+
         loss_history = list()
         optimizer = torch.optim.Adam(self.get_trainable_params(), lr=learning_rate)
         avg_time_per_epoch = 0
@@ -160,7 +197,7 @@ class FullQuantumModel(Module):
 
                     optimizer.zero_grad()
 
-                    output = self.forward(data)
+                    output = self.forward(state=data, num_layers_to_execute=num_layers_to_execute)
 
                     loss = loss_function(output, targets)
 
@@ -187,3 +224,6 @@ class FullQuantumModel(Module):
             print("--------------------------------------------------------------------------")
 
         return avg_time_per_epoch / epochs, loss_history
+
+    def fit_until(self):
+        pass
